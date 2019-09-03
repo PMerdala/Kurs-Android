3b3111f2cc707f8dbce58ef8301999b440a549a6
Paweł Merdała <p.merdala@interia.pl>
2019-09-03 21:44:14 +0200

    Dodanie niezbędnych funkcji do AppComponent

--- app/src/main/java/pl/pmerdala/kursandroid/feature/utils/injection/AppInjection.kt
+++ app/src/main/java/pl/pmerdala/kursandroid/feature/utils/injection/AppInjection.kt
@@ -1,15 +1,31 @@
 package pl.pmerdala.kursandroid.feature.utils.injection
 
+import android.app.Application
+import dagger.BindsInstance
 import dagger.Component
 import dagger.Module
+import dagger.android.AndroidInjectionModule
+import pl.pmerdala.kursandroid.BaseApplication
 
 @Module
 class AppModule {
 
 }
 
-@Component(modules = [AppModule::class])
+@Component(
+    modules = [
+        AndroidInjectionModule::class,
+        AppModule::class]
+)
 interface AppComponent {
 
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun bindApplication(app: Application): Builder
+        fun build():AppComponent
+    }
+
+    fun inject(app:BaseApplication)
 }
 
