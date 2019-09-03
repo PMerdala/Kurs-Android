b2d8a5cfbfb712cb8ee87f9bc95512f623c80958
Paweł Merdała <p.merdala@interia.pl>
2019-09-03 20:09:26 +0200

    dodanielogu do SplashActivity

--- app/src/main/java/pl/pmerdala/kursandroid/feature/splash/ui/SplashActivity.kt
+++ app/src/main/java/pl/pmerdala/kursandroid/feature/splash/ui/SplashActivity.kt
@@ -6,6 +6,7 @@ import pl.pmerdala.kursandroid.R
 import pl.pmerdala.kursandroid.feature.splash.SplashContract
 import pl.pmerdala.kursandroid.feature.splash.navigation.SplashRouter
 import pl.pmerdala.kursandroid.feature.splash.presentation.SplashPresenter
+import timber.log.Timber
 
 class SplashActivity:AppCompatActivity(),SplashContract.View {
 
@@ -13,6 +14,7 @@ class SplashActivity:AppCompatActivity(),SplashContract.View {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        Timber.d("SplashAvtivity onCreate done")
     }
 
     override fun onDestroy() {
