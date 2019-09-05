e9383304634aca386d34ad129e8586b4c8316a87
Paweł Merdała <Pawel.Merdala@adamed.com>
2019-09-05 17:41:04 +0200

    użycie Hawk do odczytu konfiguracji

--- app/src/main/java/pl/pmerdala/kursandroid/feature/utils/configuration/ConfigurationImpl.kt
+++ app/src/main/java/pl/pmerdala/kursandroid/feature/utils/configuration/ConfigurationImpl.kt
@@ -1,16 +1,14 @@
 package pl.pmerdala.kursandroid.feature.utils.configuration
 
-import android.content.SharedPreferences
+import com.orhanobut.hawk.Hawk
 
-class ConfigurationImpl(
-    private val sharedPreferences: SharedPreferences
-) : Configuration {
+class ConfigurationImpl : Configuration {
 
     companion object{
         const val PREFS_KEY_LOGGED_IN = "IsUserLoggedIn"
     }
 
     override fun isUserLoggedIn(): Boolean {
-        return sharedPreferences.getBoolean(PREFS_KEY_LOGGED_IN, false)
+        return Hawk.get<Boolean>(PREFS_KEY_LOGGED_IN, false)
     }
 }
